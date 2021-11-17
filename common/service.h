9d29bc36353520f8053f1d49caff942884df0a41
MarcoPerri <52700965+MarcoPerri@users.noreply.github.com>
2021-11-17 12:22:42 +0100

    Add project files.

---
+++ common/service.h
@@ -0,0 +1,13 @@
+#ifndef __INC_SERVICE_H__
+#define __INC_SERVICE_H__
+
+#define ENABLE_PORT_SECURITY
+#define _IMPROVED_PACKET_ENCRYPTION_ // 패킷 암호화 개선
+//#define __AUCTION__
+#define __PET_SYSTEM__
+#define __UDP_BLOCK__
+
+// V1
+//#define ENABLE_OLDHACKSHIELD // not supported anymore since 2015
+
+#endif
